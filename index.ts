86d11a8c689b05e04c99af466df4276f739d2315
Tim Bedford <tmbdfd@gmail.com>
2021-02-16 16:00:29 -0500

    Read local file to store array of all band names

--- index.ts
+++ index.ts
@@ -1,4 +1,9 @@
 import express from 'express';
+import fs from 'fs';
+
+const data = fs.readFileSync('data.txt');
+const bandnames: string[] = data.toString().split("\n")
+  .map((s: string) => s.trim());
 
 const app = express();
 const PORT = 8000;
@@ -6,6 +11,7 @@ const PORT = 8000;
 app.get('/api/query/:text', (req, res) => {
   return res.send(`You said ${req.params.text}`)
 });
+
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
 });
